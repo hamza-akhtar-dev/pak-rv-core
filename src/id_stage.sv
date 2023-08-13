5dd852e368407bb6b9239b3e22072dc607ea7986
Hamza Akhtar <hamzaakhtar8812@gmail.com>
2023-08-13 20:21:27 +0500

    code cleaned up

--- src/id_stage.sv
+++ src/id_stage.sv
@@ -1,25 +1,29 @@
-module id_stage # (
-    parameter DW = 32
-)(
-    input  logic          clk,
-    input  logic          arst_n,
-    input  logic [DW-1:0] instruction,
-    output logic [ 6:0]   opcode,
-    output logic [ 4:0]   rd,
-    output logic [ 4:0]   rs1,
-    output logic [ 4:0]   rs2,
-    output logic [ 6:0]   funct7,
-    output logic [ 2:0]   funct3,
-    output logic          imm_sign_ext,
-    output logic [31:0]   imm
+// Instruction Decode Stage
+
+module id_stage 
+# (
+    parameter DATA_WIDTH = 32
+) (
+    input  logic                  clk,
+    input  logic                  arst_n,
+    input  logic [DATA_WIDTH-1:0] instruction,
+    output logic [           6:0] opcode,
+    output logic [           4:0] rd,
+    output logic [           4:0] rs1,
+    output logic [           4:0] rs2,
+    output logic [           6:0] funct7,
+    output logic [           2:0] funct3,
+    output logic                  imm_sign_ext,
+    output logic [          31:0] imm
 );
-    assign opcode       = instruction[6:0];
-    assign rd           = instruction[11:7];
+
+    assign opcode       = instruction[ 6: 0];
+    assign rd           = instruction[11: 7];
     assign rs1          = instruction[19:15];
     assign rs2          = instruction[24:20];
     assign funct7       = instruction[31:25];
     assign funct3       = instruction[14:12];
-    assign imm_sign_ext = instruction[31];
+    assign imm_sign_ext = instruction[   31];
     assign imm          = { {20{imm_sign_ext}}, instruction[31:20] };
     
 endmodule
--- src/if_stage.sv
+++ src/if_stage.sv
@@ -1,23 +1,25 @@
+// Instruction Fetch Stage
 
-module if_stage # (
-    parameter DW            = 32,
-    parameter IMEM_SZ_IN_KB = 1,
-    localparam PC_SIZE      = $clog2(IMEM_SZ_IN_KB*1024*8)
-)(
-    input  logic               clk,
-    input  logic               arst_n,
-    input  logic [PC_SIZE-1:0] pc,
-    output logic [DW-1:0]      instruction
+module if_stage 
+# (
+    parameter  DATA_WIDTH    = 32,
+    parameter  IMEM_SZ_IN_KB = 1,
+    localparam PC_SIZE       = $clog2(IMEM_SZ_IN_KB*1024*8)
+) (
+    input  logic                  clk,
+    input  logic                  arst_n,
+    input  logic [   PC_SIZE-1:0] pc,
+    output logic [DATA_WIDTH-1:0] instruction
 );
 
-    logic [DW-1:0] instruction_memory [IMEM_SZ_IN_KB*1024*8/4]; // Example instruction memory, /4 to get num of words
+    logic [DATA_WIDTH-1:0] instruction_memory [IMEM_SZ_IN_KB*1024*8/4]; // Example instruction memory, /4 to get num of words
 
     initial
     begin
         $readmemh("../verif/gen_machine_codes/build/machine_code.mem", instruction_memory);
     end
 
-    always_ff @(posedge clk or negedge arst_n)
+    always_ff @(posedge clk, negedge arst_n)
     begin
         if (~arst_n)
         begin
